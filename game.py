211d0a52e2b2167ae74e451dbe8166af8e95db4f
greenfrog10 <100206121+greenfrog10@users.noreply.github.com>
2022-09-28 22:11:08 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from turtle import update
 from bullet import Bullet
-from player import *
+from gun import Gun
+from player import Player
 import pygame
 import window_settings
 pygame.init()
@@ -8,10 +9,12 @@ screen = pygame.display.set_mode((window_settings.width,window_settings.height))
 pygame.display.set_caption(window_settings.title)
 pygame.display.set_icon(window_settings.icon)
 player = Player("assets/hen.png",0,window_settings.height - 70)
-#bullet = Bullet(0,0,player.direction)
+gun = Gun("assets/gun_down_right.png",player.x + 60,player.y) 
+bullet = Bullet(gun.x,gun.y,player.direction,player.gun)
 run = True
 while run:
-    #bullet = Bullet(0,0,"assets/bullet.png",player.direction)
+    if bullet.fire == False:
+        bullet = Bullet(gun.x,gun.y,player.direction,player.gun)
     screen.fill(window_settings.bg_color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -21,15 +24,44 @@ while run:
                 player.left()
             if event.key == pygame.K_RIGHT:
                 player.right()
+            if event.key == pygame.K_SPACE:
+                bullet.shoot()
+            if event.key == pygame.K_UP:
+                player.raise_gun()
+            if event.key == pygame.K_DOWN:
+                player.lower_gun()
         if event.type == pygame.KEYUP:
             player.stop()
     screen.fill(window_settings.bg_color)
     player.update()
-    #bullet.update()
+    bullet.update()
     if player.x <= window_settings.border_left:
         player.x = window_settings.border_left
     elif player.x > window_settings.border_right:
         player.x = window_settings.border_right
+    if bullet.x <= window_settings.border_left:
+        bullet.fire = False
+    elif bullet.x > window_settings.border_right:
+        bullet.fire = False
+    if player.gun == "Down":
+        if player.direction == "Right":
+            gun.sprite = "assets/gun_down_right.png"
+        elif player.direction == "Left":
+            gun.sprite = "assets/gun_down_left.png"
+    if player.gun == "Up":
+        if player.direction == "Right":
+            gun.sprite = "assets/gun_up_right.png"
+        elif player.direction == "Left":
+            gun.sprite = "assets/gun_up_left.png"
     screen.blit(player.sprite,(player.x,player.y))
-    #screen.blit(bullet.sprite,(bullet.x,bullet.y))
+    if player.direction == "Right":
+        gun.x = player.x + 60
+        screen.blit(pygame.image.load(gun.sprite),(gun.x,gun.y))
+    if player.direction == "Left":
+        gun.x = player.x - 30
+        screen.blit(pygame.image.load(gun.sprite),(gun.x,gun.y))
+    if bullet.y < 0:
+        bullet.fire = False
+    if bullet.fire:
+        screen.blit(bullet.sprite,(bullet.x,bullet.y))
     pygame.display.update()
